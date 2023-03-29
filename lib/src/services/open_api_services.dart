9fc4c55f64b17c76ef9c680fa271f978cbb564a0
ay-india <ashishyd240@gmail.com>
2023-03-30 03:46:38 +0530

    pintoo

--- lib/src/services/open_api_services.dart
+++ lib/src/services/open_api_services.dart
@@ -2,59 +2,61 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:pintu/src/utils/secrets.dart';
-class AIServices{
-final List<Map<String, String>> messages = [];
 
-Future<String> checkImageOrNot(String prompt) async {
-  try {
-    final res = await http.post(
-      Uri.parse(
-        'https://api.openai.com/v1/chat/completions',
-      ),
-      headers: {
-        'Content-Type': 'applicaton/json',
-        'Authorization': 'Bearer $OPENAIAPIKEY'
-      },
-      body: jsonEncode(
-        {
-          'model': 'gpt-3.5-turbo',
-          "messages": [
-            {
-              'role': 'user',
-              'content':
-                  'Does this message want to generate an AI picture, image, art or anything similar? $prompt . Simply answer with a yes or no.',
-            }
-          ],
+class AIServices {
+  final List<Map<String, String>> messages = [];
+
+  Future<String> checkImageOrNot(String prompt) async {
+    try {
+      final res = await http.post(
+        Uri.parse(
+          'https://api.openai.com/v1/chat/completions',
+        ),
+        headers: {
+          'Content-Type': 'applicaton/json',
+          'Authorization': 'Bearer $OPENAIAPIKEY'
         },
-      ),
-    );
-    print(res.body);
+        body: jsonEncode(
+          {
+            'model': 'gpt-3.5-turbo',
+            "messages": [
+              {
+                'role': 'user',
+                'content':
+                    'Does this message want to generate an AI picture, image, art or anything similar? $prompt . Simply answer with a yes or no.',
+              }
+            ],
+          },
+        ),
+      );
+      print(res.body);
 
-    if (res.statusCode == 200) {
-      String content = jsonDecode(res.body)['choices'][0]['message']['content'];
-      content = content.trim();
+      if (res.statusCode == 200) {
+        String content =
+            jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim();
 
-      switch (content) {
-        case 'Yes':
-        case 'yes':
-        case 'Yes.':
-        case 'yes.':
-          final res = await dallEAI(prompt);
-          return res;
-        default:
-          final res = await chatGPT(prompt);
-          return res;
+        switch (content) {
+          case 'Yes':
+          case 'yes':
+          case 'Yes.':
+          case 'yes.':
+            final res = await dallEAI(prompt);
+            return res;
+          default:
+            final res = await chatGPT(prompt);
+            return res;
+        }
       }
+      return 'An internal error occurred';
+    } catch (e) {
+      print(e.toString());
+      return e.toString();
     }
-    return 'An internal error occurred';
-  } catch (e) {
-    print(e.toString());
-    return e.toString();
   }
-}
 
-Future<String> chatGPT(String prompt) async {
-  messages.add({
+  Future<String> chatGPT(String prompt) async {
+    messages.add({
       'role': 'user',
       'content': prompt,
     });
@@ -86,10 +88,39 @@ Future<String> chatGPT(String prompt) async {
     } catch (e) {
       return e.toString();
     }
-}
+  }
 
-Future<String> dallEAI(String prompt) async {
-  return '';
-}
+  Future<String> dallEAI(String prompt) async {
+    messages.add({
+      'role': 'user',
+      'content': prompt,
+    });
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/images/generations'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $OPENAIAPIKEY',
+        },
+        body: jsonEncode({
+          'prompt': prompt,
+          'n': 1,
+        }),
+      );
 
+      if (res.statusCode == 200) {
+        String imageUrl = jsonDecode(res.body)['data'][0]['url'];
+        imageUrl = imageUrl.trim();
+
+        messages.add({
+          'role': 'assistant',
+          'content': imageUrl,
+        });
+        return imageUrl;
+      }
+      return 'An internal error occurred';
+    } catch (e) {
+      return e.toString();
+    }
+  }
 }
